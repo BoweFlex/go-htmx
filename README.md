e6883dc2c3b0ca05d71ebddbb6379205f23c1b48
jbowe <jbowe@webstaurantstore.com>
2024-03-05 16:12:04 -0500

    I know how to copy paste

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # go-htmx
+
+This is meant to be a basic example of a dynamic site using the go+htmx stack. If/when I decide on a specific application for this, I will most likely rename the repo.
+
+This is a pretty blatant clone of awesome_club's [go-htmx project](https://github.com/awesome-club/go-htmx). I wanted to follow what he had done [here]() to make sure I understood the basics, then expand it into my own thing.
+Loose plan from here is:
+- Change to use tailwind for styling
+- Build homepage (BoweGo?)
+- Change existing stocks stuff to subpath under homepage
+- Build out other subpaths, i.e:
+  - [books](https://openlibrary.org/developers/api)
+  - [exchange rates](https://github.com/BoweFlex/CurrencyConverter)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+    "fmt"
+    "io" 
+    "flag" // Allows handling named CLI parameters, easier to work with than os.Args
+    "net/http"
+    "log"
+    "html/template"
+)
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "Hello From BoweFlex's http server\n")
+    io.WriteString(w, r.Method)
+}
+
+func main() {
+    var port int
+    var host string
+
+    flag.IntVar(&port, "port", 1234, "Provide a port number")
+    flag.StringVar(&host, "host", "", "Provide a host")
+
+    flag.Parse()
+
+    fullHost := fmt.Sprintf("%v:%v", host, port)
+
+    http.Handle("/static/",
+        http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+        tmpl.Execute(w, nil)
+    })
+
+    http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+        tmpl := template.Must(template.ParseFiles("./templates/fragments/results.html"))
+        data := map[string][]Stock {
+            "Results": SearchTicker(r.URL.Query().Get("key")),
+        }
+        tmpl.Execute(w, data)
+    })
+
+    http.HandleFunc("/stock/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "POST":
+			ticker := r.PostFormValue("ticker")
+			stk := SearchTicker(ticker)[0]
+			val := GetDailyValues(ticker)
+			tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+			tmpl.ExecuteTemplate(w, "stock-element",
+				Stock{Ticker: stk.Ticker, Name: stk.Name, Price: val.Open})
+		}
+	})
+
+    log.Println("Listening on ", fullHost)
+    log.Fatal(http.ListenAndServe(fullHost, nil))
+
+}
